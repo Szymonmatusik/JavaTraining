98943945ad188b1d5adf6d5e30cb8fd60611da38
Szymon <szymonmatusik94@gmail.com>
2023-04-29 21:45:13 +0200

    Finished lesson about text files handling
    Changes:
    1. Added new content to OtherTopcsMain class so as to learn how to deal
       with text files in JAVA

--- src/OtherTopics/OtherTopicsMain.java
+++ src/OtherTopics/OtherTopicsMain.java
@@ -1,5 +1,6 @@
 package OtherTopics;
 
+import java.io.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -11,7 +12,7 @@ public class OtherTopicsMain {
         int numerator, denominator;
         int divisionResult;
 
-        while (true) {
+       /* while (true) {
             //try division and catch possible errors
             try {
                 System.out.print("Enter a whole number to divide: ");
@@ -34,6 +35,67 @@ public class OtherTopicsMain {
             finally {
                 scanner.nextLine(); //in case input is not correct
             }
+        }
+
+        */
+
+        //File class (writing and reading from file)
+        File file = new File("C:\\Users\\Szymon\\Desktop\\JAVA\\BroCode\\src\\OtherTopics\\text.txt");
+        if (file.exists()){
+            System.out.println("That file exists");
+            System.out.println(file.getPath());
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.isFile());
+            //System.out.println(file.delete());
+        }
+        else{
+            System.out.println("That file does not exist.");
+        }
+
+        //Writing to a File
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write("Hello, I'm testing file writing!\nAdding a new line\nTrying something new\n");
+            fileWriter.append("Trying to append some new stuff");
+            System.out.println("Im here");
+            fileWriter.close();
+        }
+        catch (IOException e){
+            System.out.println("No such file!");
+        }
+
+        //Reading from a file
+        //1st method
+        char[] fileContent = new char[(int)file.length()];
+        try{
+            FileReader fileReader = new FileReader(file);
+
+            if (file.isFile()){
+                fileReader.read(fileContent);
+            }
+
+            System.out.println(fileContent);
+        }
+        catch (IOException e){
+            System.out.println("No such file or directory!");
+        }
+
+        System.out.println();
+
+        //2nd method
+        String readContentNew;
+        try {
+            FileReader fileReaderNew = new FileReader(file);
+            int data = fileReaderNew.read();
+
+            while (data != -1){
+                System.out.print((char)data);
+                data = fileReaderNew.read();
+            }
+            fileReaderNew.close();
+        }
+        catch (IOException e) {
+            System.out.println("No such file!");
         }
     }
 }
