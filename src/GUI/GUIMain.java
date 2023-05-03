fd2617ffd0fe046f0c8c5dd777c90b0ba4a05e8f
Szymon <szymonmatusik94@gmail.com>
2023-05-03 18:50:48 +0200

    Finished part devoted buttons in JAVA GUI
    Changes:
    1. Code was refatored - all stuff from main was moved to MyFrame()
       constructor so as to clean the code a little bit.
    2. Created new button and label. Pressing one buttons shows the label
       whilst pressing another hides it.
    3. In GUI Main whole irrelevant code was deleted and moved to the
       MyFrame class.

--- src/GUI/GUIMain.java
+++ src/GUI/GUIMain.java
@@ -15,7 +15,7 @@ public class GUIMain {
                                     "C:\\Users\\Szymon\\Desktop\\JAVA\\BroCode\\src\\GUI\\img\\icon.jpg");
         label1.setVerticalAlignment(JLabel.BOTTOM);
         label1.setHorizontalAlignment(JLabel.RIGHT);
-         */
+
 
         String iconPath = "C:\\Users\\Szymon\\Desktop\\JAVA\\BroCode\\src\\GUI\\img\\icon.jpg";
         ImageIcon imageIcon = new ImageIcon(new ImageIcon(iconPath).getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
@@ -27,45 +27,8 @@ public class GUIMain {
         label1.setHorizontalTextPosition(JLabel.LEFT);
         label1.setFont(new Font("MV Boli", Font.PLAIN, 10)); //set font of text
 
-
-        JPanel redPanel = new MyJPanel(Color.RED,0, 0,250 , 250 );
-        redPanel.setLayout(null);
-
-        JPanel bluePanel = new MyJPanel(Color.BLUE, redPanel.getX() + redPanel.getWidth(), 0, 250, 250);
-        bluePanel.setLayout(null);
-
-        JPanel greenPanel = new MyJPanel(Color.GREEN, (redPanel.getWidth()+bluePanel.getWidth())/4,
-                                                            redPanel.getHeight(), 250, 250);
-        greenPanel.setLayout(null);
-
-        //redPanel.setLayout(new BorderLayout());
-
-        //Buttons
-        ImageIcon buttonIcon = new ImageIcon(new ImageIcon("C:\\Users\\Szymon\\Desktop\\JAVA\\BroCode\\src\\GUI\\img\\poop.png")
-                                                            .getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH));
-        JButton button = new JButton();
-        button.setLayout(null);
-        button.setBounds(75, 100,
-                        100, 50);
-        button.setText("Click here!");
-        button.setIcon(buttonIcon);
-        button.setHorizontalTextPosition(JButton.CENTER);
-        button.setFocusable(false);
-
-
-
-
-        MyFrame frame = new MyFrame(button);
-        frame.setSize(400, 400);
-        frame.setLayout(null);
-        frame.add(redPanel);
-        frame.add(bluePanel);
-        frame.add(greenPanel);
-        redPanel.add(label1);
-        greenPanel.add(button);
-        //frame.pack();
-        frame.setVisible(true);
-        //frame.pack();
+         */
+        new MyFrame();
 
     }
 }
--- src/GUI/MyFrame.java
+++ src/GUI/MyFrame.java
@@ -1,34 +1,112 @@
 package GUI;
 
-import AccessModifiersPackage2.B;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 public class MyFrame extends JFrame {
 
-    JButton button;
-    MyFrame(JButton button){
-        //JFrame = a GUI Window to add components to
-        this.button = button;
-        //lambda expression!!!
-        this.button.addActionListener(e -> {
+    JButton buttonShowIcon, buttonHideIcon;
+    JLabel label;
+    JPanel redPanel, greenPanel, bluePanel;
+    MyFrame(){
+        //Frame icon
+        ImageIcon frameIcon = new ImageIcon("C:\\Users\\Szymon\\Desktop\\JAVA\\BroCode\\src\\GUI\\img\\icon.jpg");
+
+        //Button icon
+        ImageIcon buttonIcon = new ImageIcon(new ImageIcon ("C:\\Users\\Szymon\\Desktop\\JAVA\\BroCode\\src\\GUI\\img\\poop.png")
+                                            .getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH));
+
+        //Label icon
+        ImageIcon labelIcon = new ImageIcon(new ImageIcon("C:\\Users\\Szymon\\Desktop\\JAVA\\BroCode\\src\\GUI\\img\\funny.png")
+                                                            .getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+
+
+        //instances of frame elements
+        buttonShowIcon = new JButton(buttonIcon);
+        buttonHideIcon = new JButton(buttonIcon);
+        label = new JLabel();
+        redPanel = new JPanel();
+        bluePanel = new JPanel();
+        greenPanel = new JPanel();
+
+        //redPanel parameters
+        redPanel.setBackground(new Color(255, 0,0));
+        redPanel.setLayout(null);
+        redPanel.setBounds(0,0,250,250);
+
+        //bluePanel parameters
+        bluePanel.setBackground(new Color(0, 0,255));
+        bluePanel.setLayout(null);
+        bluePanel.setBounds(redPanel.getX() + redPanel.getWidth(),0, 250, 250);
+
+        //greenPanel parameters
+        greenPanel.setBackground(new Color(0, 255,0));
+        greenPanel.setLayout(null);
+        greenPanel.setBounds((redPanel.getWidth()+bluePanel.getWidth())/4, redPanel.getHeight(), 250, 250);
+
+        //show icon button parameters
+        buttonShowIcon.setBounds(12, 100,
+                100, 50);
+        buttonShowIcon.setText("Show label!");
+        buttonShowIcon.setIcon(buttonIcon);
+        buttonShowIcon.setHorizontalTextPosition(JButton.CENTER);
+        buttonShowIcon.setFocusable(false);
+        buttonShowIcon.setFont(new Font("Comic Sans", Font.BOLD, 10));
+        buttonShowIcon.setForeground(new Color(255, 0,0 ));
+        buttonShowIcon.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0)));
+
+        //hide icon button parameters
+        buttonHideIcon.setLayout(null);
+        buttonHideIcon.setBounds(12+buttonShowIcon.getWidth() + 25, 100,
+                100, 50);
+        buttonHideIcon.setText("Hide label!");
+        buttonHideIcon.setIcon(buttonIcon);
+        buttonHideIcon.setHorizontalTextPosition(JButton.CENTER);
+        buttonHideIcon.setFocusable(false);
+        buttonHideIcon.setFont(new Font("Comic Sans", Font.BOLD, 10));
+        buttonHideIcon.setForeground(new Color(255, 0,0 ));
+        buttonHideIcon.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0)));
+
+        //label parameters
+        label.setIcon(labelIcon);
+        label.setLayout(null);
+        label.setBounds(100, 100, 200, 200);
+        label.setVisible(false);
+
+        //lambda expression!!! - print statement on button click
+        buttonShowIcon.addActionListener(e -> {
             System.out.println("Poop");
+            label.setVisible(true);
         });
 
-        ImageIcon imageIcon = new ImageIcon("C:\\Users\\Szymon\\Desktop\\JAVA\\BroCode\\src\\GUI\\img\\icon.jpg");
+        //lambda expression!!! - print statement on button click
+        buttonHideIcon.addActionListener(e -> {
+            System.out.println("Poop hidden");
+            label.setVisible(false);
+        });
+
+
+
+
+
+
+
+
 
         this.setTitle("This is my first GUI window"); //add title to the frame window
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exits the application
         this.setResizable(true); //abandon to resize the application
-        this.setIconImage(imageIcon.getImage()); //change icon of the frame
-        //this.setSize(420, 420); //set size of the frame window
-        //this.getContentPane().setBackground(new Color(50, 0,120)); //Change color of the background
+        this.setIconImage(frameIcon.getImage()); //change icon of the frame
+        this.setSize(750, 750);
+        this.setLayout(null);
+        this.add(redPanel);
+        this.add(bluePanel);
+        this.add(greenPanel);
+        greenPanel.add(buttonShowIcon);
+        greenPanel.add(buttonHideIcon);
+        greenPanel.add(label);
+        this.setVisible(true);
 
-        //this.setVisible(true); //set frame visibility
 
     }
 }
